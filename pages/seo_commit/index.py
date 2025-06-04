df1be56456cc77048022b3f2a88c3686bc3444f2
朱毅鑫 <zhuyx1022@gmail.com>
2025-06-04 16:24:52 +0800

    提交脚本修改

--- pages/seo_commit/index.py
+++ pages/seo_commit/index.py
@@ -18,19 +18,19 @@ if response.status_code == 200:
         else:
             num = 0  # fallback
 
-        urlArray = arr[num * 10: (num * 10 + 1)]
-        url = "http://data.zz.baidu.com/urls?site=www.cnhivehub.com&token=eehiuOEZPm72Y49K"
-
-        data = "\n".join(urlArray)
-        print('本次 URL:', data)
-
-        headers = {'Content-Type': 'text/plain'}
-        baiduResponse = requests.post(url, headers=headers, data=data.encode('utf-8'))
-        print("Status Code:", baiduResponse.status_code)
-        print("Response:", baiduResponse.text)
+        # urlArray = arr[num * 10: (num * 10 + 1)]
+        # url = "http://data.zz.baidu.com/urls?site=www.cnhivehub.com&token=eehiuOEZPm72Y49K"
+        #
+        # data = "\n".join(urlArray)
+        # print('本次 URL:', data)
+        #
+        # headers = {'Content-Type': 'text/plain'}
+        # baiduResponse = requests.post(url, headers=headers, data=data.encode('utf-8'))
+        # print("Status Code:", baiduResponse.status_code)
+        # print("Response:", baiduResponse.text)
 
         # 如果成功，覆盖写入新的 dayNum
-        if baiduResponse.status_code == 200:
+        # if baiduResponse.status_code == 200:
             a.seek(0)           # 回到文件头
             a.truncate()        # 清空文件
             writer = csv.writer(a)
